9aa1d87c831d507c66e83efaec107a8888fb150c
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-04 14:34:21 +0700

    WIP

--- src/app/components/home/MyQuote.tsx
+++ src/app/components/home/MyQuote.tsx
@@ -19,15 +19,19 @@ const MyQuote = () => {
         </div>
 
         <div className="flex items-center justify-center relative">
-          <div className="max-w-sm md:max-w-md p-4 md:p-6 text-center md:text-left border-l border-black">
-            <h1 className="font-ketiga font-extrabold text-xl p-1">RIZPUTRA</h1>
-            <p className="font-pertama p-1">
-              Every process must have a purpose, every code has a meaning be it
-              an error or a bug. And every time I love I am self-aware.
-            </p>
+          <div className="max-w-sm md:max-w-md p-4 md:p-6 ">
+            <div className="p-4 text-center md:text-left border-l border-black">
+              <h1 className="font-ketiga font-extrabold text-xl p-1">
+                RIZPUTRA
+              </h1>
+              <p className="font-pertama p-1">
+                Every process must have a purpose, every code has a meaning be
+                it an error or a bug. And every time I love I am self-aware.
+              </p>
+            </div>
           </div>
 
-          <div className="w-8 h-20 border-l border-t border-black absolute -left-2 top-6"></div>
+          <div className="w-8 h-20 border-l border-t border-black absolute left-2 top-8"></div>
         </div>
       </div>
     </div>
