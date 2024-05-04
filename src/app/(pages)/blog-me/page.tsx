e0e0f42200af14a5fe2a1a341a34d1bb5f691f90
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-04 17:01:38 +0700

    WIP

--- src/app/(pages)/blog-me/page.tsx
+++ src/app/(pages)/blog-me/page.tsx
@@ -1,9 +1,14 @@
+import Blog from "@/app/components/blog/Blog";
 import React from "react";
 
 const page = () => {
   return (
     <main className="flex flex-col items-center justify-center">
-      <section className="py-8 md:py-16 px-4"></section>
+      <section className="max-w-7xl mx-auto py-6 md:py-12">
+        <div className="flex items-center justify-center">
+          <Blog />
+        </div>
+      </section>
     </main>
   );
 };
