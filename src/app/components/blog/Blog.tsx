154ca332ee37d6fab79290cb763fa206a84af406
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-06 14:42:57 +0700

    WIP

--- src/app/components/blog/Blog.tsx
+++ src/app/components/blog/Blog.tsx
@@ -12,7 +12,7 @@ const Blog = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center justify-center">
-        <div className="w-full">
+        <div className="w-full mb-6">
           <div className="flex items-center justify-between gap-2 md:gap-6 xl:gap-10 mb-6">
             {sampulBlog.map((sampul, urutan) => (
               <div
@@ -28,20 +28,57 @@ const Blog = () => {
             ))}
           </div>
 
-          <div className="flex flex-col md:flex-row items-center">
-            <div className="p-2 md:p-6">
-              <h1 className="font-kedua font-extrabold text-3xl">BLOG ME</h1>
+          <div className="relative p-4 self-start">
+            <div className="max-w-xs p-4 border-l border-black">
+              <p className="font-pertama">
+                I recorded all the activities, whether ideas, quotes or about
+                joys or sorrows here.
+              </p>
             </div>
 
-            <div className="relative p-4 self-start">
-              <div className="max-w-xs p-4 border-l border-black">
-                <p className="font-pertama">
-                  I recorded all the activities, whether ideas, quotes or about
-                  joys or sorrows here.
-                </p>
+            <div className="w-8 h-20 border-l border-t border-black absolute left-2 top-6"></div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div className="p-4 max-w-2xl text-center">
+            <h1 className="font-kedua font-extrabold text-3xl">BLOGS</h1>
+            <p className="font-pertama text-lg p-2">
+              Collection of my diaries.
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-row items-center md:items-start justify-center">
+            <div className="p-4">
+              <div className="p-2">
+                <h1 className="font-ketiga font-extrabold text-lg">
+                  BETWEEN LOVE AND CODE
+                </h1>
               </div>
 
-              <div className="w-8 h-20 border-l border-t border-black absolute left-2 top-6"></div>
+              <div className="max-w-md p-2">
+                <p className="font-pertama">
+                  Destiny is the structure in the code of life, life is a
+                  dynamic interface, and love is a design element that gives
+                  color to every experience.
+                </p>
+              </div>
+            </div>
+
+            <div className="p-4">
+              <div className="p-2">
+                <h1 className="font-ketiga font-extrabold text-lg">
+                  SHINE YOURSELF
+                </h1>
+              </div>
+
+              <div className="max-w-md p-2">
+                <p className="font-pertama">
+                  Sometimes, we have to be a source of strength for ourselves.
+                  Start today with the belief that you have the power to change
+                  the world around you.
+                </p>
+              </div>
             </div>
           </div>
         </div>
