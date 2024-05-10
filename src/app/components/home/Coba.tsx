24a6fb7fc55829a9f0069f9b832a10aeb0220b9d
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-10 19:14:13 +0700

    WIP

--- src/app/components/home/Coba.tsx
+++ src/app/components/home/Coba.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 const Coba = () => {
@@ -10,13 +11,17 @@ const Coba = () => {
     {
       foto: "https://fastly.picsum.photos/id/501/900/500.jpg?hmac=C86Xc0e9WvJ3ZqwwE-7nHdH7_knINAIw-1IULXSusN4",
       judul: "I AM",
-      subJudul: "RIZKY PUTRA",
+      subJudul: "RIZPUTRA",
+      btnJudul: "INTRODUCE",
+      link: "/about-me",
     },
 
     {
       foto: "https://fastly.picsum.photos/id/281/900/500.jpg?hmac=kgHy9BI0bU8yCwI-T_otZvnibeVWjQbigaH2VwvqPD4",
       judul: "CREAVITY",
       subJudul: "ABOVE ALL",
+      btnJudul: "LOOK NOW",
+      link: "/blogs",
     },
   ];
   const latarBelakang = ["/bg/bg1.jpg", "/bg/bg2.jpg"];
@@ -90,13 +95,13 @@ const Coba = () => {
       <div className="flex items-center justify-center">
         <div className="relative flex items-center justify-center">
           <div
-            className={`relative md:absolute md:top-10 md:-right-14 transition-transform duration-500 ${
+            className={`relative md:absolute md:top-10 md:-right-14 lg:top-12 lg:-right-52 xl:-right-80 transition-transform duration-500 ${
               animasiGeser
                 ? "-translate-x-2 md:-translate-x-4"
                 : "translate-x-0"
             }`}
           >
-            <div className="w-full h-60 md:w-[450px] md:h-80">
+            <div className="w-full h-60 md:w-[450px] md:h-80 lg:w-[500px] lg:h-[400px] xl:w-[740px] xl:h-[500px]">
               <img
                 src={gambar[titikSaatIni].foto}
                 alt="me"
@@ -106,11 +111,11 @@ const Coba = () => {
           </div>
 
           <div
-            className={`absolute md:relative top-[2px] -z-10 right-0 md:-right-32 md:top-0 transition-transform duration-500 ${
+            className={`absolute md:relative top-[2px] -z-10 right-0 md:-right-32 md:top-0 lg:-right-72 xl:-right-[440px] transition-transform duration-500 ${
               animasiGeser ? "translate-x-2 md:translate-x-4" : "translate-x-0"
             }`}
           >
-            <div className="w-64 h-[300px] md:w-[350px] md:h-[400px]">
+            <div className="w-64 h-[300px] md:w-[350px] md:h-[400px] lg:w-[400px] lg:h-[500px] xl:w-[420px] xl:h-[600px]">
               <img
                 src={latarBelakang[titikSaatIni]}
                 alt="Background"
@@ -120,22 +125,31 @@ const Coba = () => {
           </div>
 
           <div
-            className={`absolute left-0 -bottom-11 md:-bottom-28 md:-left-36 ease-in-out duration-1000 ${
+            className={`absolute left-0 -bottom-11 md:-bottom-32 md:-left-44 lg:top-32 lg:-left-72 xl:-left-[380px] ease-in-out duration-1000 ${
               animasiGeser ? "opacity-0" : " opacity-100"
             }`}
           >
             <div className="p-2">
-              <h1 className="text-4xl md:text-6xl font-kedua font-extrabold">
+              <h1 className="text-4xl md:text-6xl lg:text-8xl font-kedua font-extrabold">
                 {gambar[titikSaatIni].judul}
               </h1>
 
-              <h2 className="text-4xl md:text-6xl font-kedua font-extrabold">
+              <h2 className="text-4xl md:text-6xl lg:text-8xl font-kedua font-extrabold">
                 {gambar[titikSaatIni].subJudul}
               </h2>
             </div>
+
+            <div className="mx-10 my-2 md:my-4">
+              <Link
+                href={gambar[titikSaatIni].link}
+                className="py-2 px-6 bg-black text-white font-kedua text-2xl"
+              >
+                {gambar[titikSaatIni].btnJudul}
+              </Link>
+            </div>
           </div>
 
-          <div className="absolute -bottom-10 right-10 md:-bottom-20 md:-right-10 flex items-center justify-center gap-2">
+          <div className="absolute -bottom-10 right-10 md:-bottom-20 md:-right-10 lg:bottom-0 lg:right-[420px] xl:bottom-28 xl:right-[500px] flex items-center justify-center gap-2">
             {gambar.map((_, titik) => (
               <div
                 key={titik}
@@ -149,6 +163,18 @@ const Coba = () => {
                 <div className="w-2 h-2 bg-white md:bg-black rounded-full"></div>
               </div>
             ))}
+          </div>
+
+          <div
+            className={`absolute -z-20 -bottom-24 -left-2 md:-bottom-16 md:-left-28 lg:-bottom-24 lg:left-8 xl:-top-10 xl:left-28 ease-in-out duration-1000 ${
+              animasiGeser ? "opacity-0" : " opacity-100"
+            }`}
+          >
+            <div className="w-[300px] h-[300px] md:w-[420px] md:h-[420px] lg:w-[560px] lg:h-[560px] xl:w-[680px] xl:h-[680px] border border-black rounded-full"></div>
+          </div>
+
+          <div className="absolute -bottom-20 right-12 md:-bottom-6 md:right-4 lg:-bottom-10 lg:right-40 xl:bottom-0 xl:right-60">
+            <div className="w-48 h-4 lg:w-72 xl:w-[400px] border-b border-black"></div>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,11 @@
-import Link from "next/link";
-import Square from "./components/background/Square";
-import Gallery from "./components/gallery/Gallery";
-import Expression from "./components/home/Expression";
-import Hero from "./components/home/Hero";
-import IPage from "./components/home/IPage";
-import Me from "./components/home/Me";
-import MyQuote from "./components/home/MyQuote";
-import Plus from "./components/background/Plus";
-import Wave from "./components/background/Wave";
+import Coba from "./components/home/Coba";
 
 export default function Home() {
   return (
     <main>
-      <section className="mt-6 mb-10 md:my-12 lg:my-16 xl:my-20">
-        <div className="relative flex items-center justify-center">
-          <div className="flex flex-col lg:flex-row items-center justify-center lg:-space-x-40 xl:space-x-20">
-            <div className="order-2 lg:order-1">
-              <Hero />
-            </div>
-
-            <div className="order-1 lg:order-2 lg:-z-10 xl:z-0">
-              <Me />
-            </div>
-          </div>
-
-          <div className="absolute lg:left-40 lg:top-28 xl:top-24 xl:left-52 2xl:left-[450px] -z-40">
-            <Square />
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <Plus />
-      </section>
-
-      <section className="my-10 md:my-12 lg:my-16 xl:my-20">
+      <section className="my-0 md:my-20">
         <div className="flex items-center justify-center">
-          <IPage />
-        </div>
-      </section>
-
-      <section className="my-10 md:my-12 lg:my-16 xl:my-20">
-        <div className="flex items-center justify-center">
-          <Expression />
-        </div>
-      </section>
-
-      <section>
-        <Wave />
-      </section>
-
-      <section className="my-10 md:my-12 lg:my-16 xl:my-20">
-        <div className="flex flex-col items-center justify-center">
-          <div className="hidden xl:block">
-            <Gallery batasGambar={8} tampilkanSampul={false} />
-          </div>
-
-          <div className="block xl:hidden">
-            <Gallery batasGambar={6} tampilkanSampul={false} />
-          </div>
-
-          <Link
-            href="/gallery"
-            className="px-6 py-2 bg-black text-white font-ketiga font-extrabold my-4"
-          >
-            SHOW ALL
-          </Link>
-        </div>
-      </section>
-
-      <section className="my-10 md:my-12 lg:my-16 xl:my-20">
-        <div className="flex items-center justify-center">
-          <MyQuote />
+          <Coba />
         </div>
       </section>
     </main>
