6a50c58e62808da8c67324c30d4675b8f5d6f776
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-10 07:07:33 +0700

    WIP

--- src/app/components/home/Coba.tsx
+++ src/app/components/home/Coba.tsx
@@ -8,15 +8,15 @@ const Coba = () => {
 
   const gambar = [
     {
-      foto: "https://fastly.picsum.photos/id/741/500/500.jpg?hmac=QxgEVGp1dAJW9qBYyKqxns1xKcymqoc16utopq1Osck",
+      foto: "https://fastly.picsum.photos/id/501/900/500.jpg?hmac=C86Xc0e9WvJ3ZqwwE-7nHdH7_knINAIw-1IULXSusN4",
       judul: "I AM",
       subJudul: "RIZKY PUTRA",
     },
 
     {
-      foto: "https://fastly.picsum.photos/id/468/500/500.jpg?hmac=HyiMmiWqLT84T6ekFJ7STK5l1yFF45gSkaafBtFQcyc",
-      judul: "FRONT-END",
-      subJudul: "WEB DEVELOPER",
+      foto: "https://fastly.picsum.photos/id/281/900/500.jpg?hmac=kgHy9BI0bU8yCwI-T_otZvnibeVWjQbigaH2VwvqPD4",
+      judul: "CREAVITY",
+      subJudul: "ABOVE ALL",
     },
   ];
   const latarBelakang = ["/bg/bg1.jpg", "/bg/bg2.jpg"];
@@ -82,49 +82,73 @@ const Coba = () => {
   };
 
   return (
-    <div className="flex items-center justify-center">
-      <div
-        className=""
-        onTouchStart={sentuhanDimulai}
-        onTouchMove={sentuhanBerpindah}
-      >
-        <div className="flex flex-col items-center justify-center">
-          <div className="relative flex items-center justify-center">
-            <div className="absolute top-0 left-10">
-              <div
-                className={`transition-transform duration-500 ${
-                  animasiGeser
-                    ? "translate-x-2 md:translate-x-4"
-                    : "-translate-x-0"
-                }`}
-              >
-                <div className="w-52 h-72">
-                  <img
-                    src={latarBelakang[titikSaatIni]}
-                    alt="Background"
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-              </div>
+    <div
+      className="flex items-center justify-center"
+      onTouchStart={sentuhanDimulai}
+      onTouchMove={sentuhanBerpindah}
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative flex items-center justify-center">
+          <div
+            className={`relative md:absolute md:top-10 md:-right-14 transition-transform duration-500 ${
+              animasiGeser
+                ? "-translate-x-2 md:-translate-x-4"
+                : "translate-x-0"
+            }`}
+          >
+            <div className="w-full h-60 md:w-[450px] md:h-80">
+              <img
+                src={gambar[titikSaatIni].foto}
+                alt="me"
+                className="object-cover w-full h-full"
+              />
             </div>
+          </div>
 
-            <div className="absolute top-0 -right-40">
-              <div
-                className={`transition-transform duration-500 ${
-                  animasiGeser
-                    ? "-translate-x-2 md:-translate-x-4"
-                    : "translate-x-0"
-                }`}
-              >
-                <div className="w-96 h-64">
-                  <img
-                    src={gambar[titikSaatIni].foto}
-                    alt="me"
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-              </div>
+          <div
+            className={`absolute md:relative top-[2px] -z-10 right-0 md:-right-32 md:top-0 transition-transform duration-500 ${
+              animasiGeser ? "translate-x-2 md:translate-x-4" : "translate-x-0"
+            }`}
+          >
+            <div className="w-64 h-[300px] md:w-[350px] md:h-[400px]">
+              <img
+                src={latarBelakang[titikSaatIni]}
+                alt="Background"
+                className="object-cover w-full h-full"
+              />
             </div>
+          </div>
+
+          <div
+            className={`absolute left-0 -bottom-11 md:-bottom-28 md:-left-36 ease-in-out duration-1000 ${
+              animasiGeser ? "opacity-0" : " opacity-100"
+            }`}
+          >
+            <div className="p-2">
+              <h1 className="text-4xl md:text-6xl font-kedua font-extrabold">
+                {gambar[titikSaatIni].judul}
+              </h1>
+
+              <h2 className="text-4xl md:text-6xl font-kedua font-extrabold">
+                {gambar[titikSaatIni].subJudul}
+              </h2>
+            </div>
+          </div>
+
+          <div className="absolute -bottom-10 right-10 md:-bottom-20 md:-right-10 flex items-center justify-center gap-2">
+            {gambar.map((_, titik) => (
+              <div
+                key={titik}
+                className={`rounded-full cursor-pointer ${
+                  titik === titikSaatIni
+                    ? "p-2 md:p-4 border border-white md:border-black"
+                    : "p-2 md:p-4"
+                }`}
+                onClick={() => gantiGambar(titik)}
+              >
+                <div className="w-2 h-2 bg-white md:bg-black rounded-full"></div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
