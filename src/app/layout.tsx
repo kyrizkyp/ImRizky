e7c923604614646646abf07045436a4cbf0f1bc0
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-16 11:59:19 +0700

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ interface CustomMetadata extends Metadata {
 }
 
 export const metadata: CustomMetadata = {
-  title: "KYRIZKYP",
+  title: "RIZKYP",
   description:
     "Introducing me Rizky Putra, a Front-End Web Developer, specialist and precision in creating websites or what is usually called landing pages.",
   image: "/logo/iam.png",
