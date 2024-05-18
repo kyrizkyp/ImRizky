0a9f81164a92764acfbb6d0fcec84782d9b79815
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-18 22:16:51 +0700

    WIP

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
   const Tahun = new Date().getFullYear();
   return (
     <div className="p-4 flex items-center justify-center">
-      <div className="text-center p-2 text-xs lg:text-base">
+      <div className="text-center p-2 text-base lg:text-lg flex flex-col md:flex-row md:gap-2 items-center justify-center">
         <h1 className="font-pertama">
           Copyright &copy; {Tahun}{" "}
           <span className="font-ketiga text-extraold">KYRIZKYP</span>,
-          Everything rights reserved.
         </h1>
+        <h2 className="font-pertama">Everything rights reserved.</h2>
       </div>
     </div>
   );
