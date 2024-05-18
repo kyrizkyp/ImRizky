99e0235c3ae71581c3300faea9132e62aae53bba
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-18 11:26:40 +0700

    WIP

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center my-4 md:my-40 xl:my-20 2xl:my-40">
+    <div className="flex flex-col items-center justify-center my-4 md:my-40 xl:my-20 2xl:my-40">
       <div className="flex flex-col xl:flex-row items-center justify-center md:gap-10 xl:gap-20">
         <div className="w-60 h-60 lg:w-80 lg:h-80 xl:w-[400px] xl:h-[400px] border border-black rounded-full flex items-center justify-center">
           <div className="text-center font-kedua font-extrabold">
@@ -14,10 +14,18 @@ const NotFound = () => {
         <div className="flex flex-col items-center justify-center p-4">
           <div className="max-w-md text-center xl:text-left">
             <h2 className="font-kedua text-4xl p-2">PAGE NOT FOUND</h2>
-            <p className="font-pertama p-2">
-              Oops! The page you are looking for does not exist. It might have
-              been moved or deleted.
-            </p>
+            <div className="p-2 relative flex items-center justify-center">
+              <div className="p-2 border-l border-black">
+                <p className="font-pertama">
+                  Oops! The page you are looking for does not exist. It might
+                  have been moved or deleted.
+                </p>
+              </div>
+
+              <div className="absolute top-0 left-0">
+                <div className="w-6 h-10 border-l border-t border-black"></div>
+              </div>
+            </div>
           </div>
 
           <div className="xl:self-start my-4 mx-2">
@@ -28,6 +36,12 @@ const NotFound = () => {
               GO BACK
             </Link>
           </div>
+        </div>
+      </div>
+
+      <div className="my-4">
+        <div className="p-4 text-4xl">
+          <h3 className="font-kedua font-extrabold">{"//////"}</h3>
         </div>
       </div>
     </div>
