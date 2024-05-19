40a7cbe92e48c637809966cf2bb6cc140c6e0006
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-19 22:14:07 +0700

    WIP

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,19 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center my-4 md:my-40 xl:my-20 2xl:my-40">
       <div className="flex flex-col xl:flex-row items-center justify-center md:gap-10 xl:gap-20">
-        <div className="w-60 h-60 lg:w-80 lg:h-80 xl:w-[400px] xl:h-[400px] border border-black rounded-full flex items-center justify-center">
-          <div className="text-center font-kedua font-extrabold">
-            <h1 className="text-6xl">404</h1>
+        <div className="relative flex items-center justify-center">
+          <div className="w-60 h-60 lg:w-80 lg:h-80 xl:w-[400px] xl:h-[400px] border border-black rounded-full flex items-center justify-center">
+            <div className="text-center font-kedua font-extrabold">
+              <h1 className="text-6xl">404</h1>
+            </div>
+          </div>
+
+          <div className="absolute top-10 left-4">
+            <div className="w-40 h-10 border-t border-black"></div>
+          </div>
+
+          <div className="absolute bottom-10 right-4">
+            <div className="w-40 h-10 border-b border-black"></div>
           </div>
         </div>
 
