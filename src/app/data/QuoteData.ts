ed6544ebe409f380a446030a8387017cbcbf185d
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-22 18:01:31 +0700

    WIP

--- src/app/data/QuoteData.ts
+++ src/app/data/QuoteData.ts
@@ -6,21 +6,21 @@ interface DataQuotes {
 
 const QuoteData: DataQuotes[] = [
   {
-    no: "1",
+    no: "01",
     judul: "BETWEEN LOVE AND CODE",
     deskripsi:
       "Destiny is the structure in the code of life, life is a dynamic interface, and love is a design element that gives color to every experience.",
   },
 
   {
-    no: "2",
+    no: "02",
     judul: "SHINE YOURSELF",
     deskripsi:
       "Sometimes, we have to be a source of strength for ourselves. Start today with the belief that you have the power to change the world around you.",
   },
 
   {
-    no: "3",
+    no: "03",
     judul: "CAT INSTINCT, EAGLE VISION",
     deskripsi:
       "Navigate space like an Eagle, guide your focus with sharpness. Instincts in the dark like a Cat, embracing change with sharp instincts.",
