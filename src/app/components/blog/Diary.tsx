6428f71475d4054fca0474e8a4d40ea30b3815ba
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-26 02:45:45 +0700

    WIP

--- src/app/components/blog/Diary.tsx
+++ src/app/components/blog/Diary.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import DiaryData from "@/app/data/DiaryData";
 import Link from "next/link";
 
+const itemPerHalaman = 4;
+
 const Diary = () => {
+  const [halamanSekarang, mengaturHalamanSekarang] = useState(1);
+
+  const totalHalaman = Math.ceil(DiaryData.length / itemPerHalaman);
+
+  const handlePageChange = (halaman: React.SetStateAction<number>) => {
+    mengaturHalamanSekarang(halaman);
+  };
+
+  const dataSaatIni = DiaryData.slice(
+    (halamanSekarang - 1) * itemPerHalaman,
+    halamanSekarang * itemPerHalaman
+  );
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="p-4 my-2 text-center">
-        <h1 className="font-kedua font-extrabold text-3xl">MY NOTES</h1>
-
-        <p className="font-pertama p-2">I note plans or inspiration here.</p>
+        <h1 className="font-kedua font-extrabold text-3xl">CATATAN SAYA</h1>
+        <p className="font-pertama p-2">
+          Saya mencatat rencana atau inspirasi di sini.
+        </p>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-2 xl:gap-16 px-4 my-2">
-        {DiaryData.map((blog, urutan) => (
+        {dataSaatIni.map((blog, urutan) => (
           <Link href={`/blogs/${blog.id}`} key={urutan}>
             <div className="p-2 border border-black">
               <div className="p-1">
@@ -20,16 +37,30 @@ const Diary = () => {
                   {blog.judul}
                 </h2>
               </div>
-
               <div className="max-w-xs p-1">
                 <p className="font-pertama">{blog.subJudul}</p>
               </div>
-
               <div className="p-2">
                 <p className="font-ketiga text-sm">{blog.status}</p>
               </div>
             </div>
           </Link>
+        ))}
+      </div>
+
+      <div className="flex justify-center my-4">
+        {Array.from({ length: totalHalaman }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => handlePageChange(i + 1)}
+            className={`m-2 ${
+              halamanSekarang === i + 1 ? "bg-black" : "bg-gray-400"
+            }`}
+          >
+            <div className="px-4 py-2">
+              <p className="font-kedua text-white">{i + 1}</p>
+            </div>
+          </button>
         ))}
       </div>
     </div>
--- src/app/data/DiaryData.ts
+++ src/app/data/DiaryData.ts
@@ -24,5 +24,69 @@ const DiaryData: DataBlog[] = [
 
     id: "bLgSOnE",
   },
+
+  {
+    judul: "LIST PLAN IN ARTWORK",
+
+    subJudul: "ini subjudul atau deskripsi singkat",
+
+    status: "COMING SOON",
+
+    desk1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae totam quos odio ex autem suscipit, nam dolorem reprehenderit ipsum itaque doloribus, alias, saepe unde?",
+
+    desk2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, enim quaerat! Porro minus obcaecati doloremque eum rem, libero aperiam consectetur exercitationem unde, nisi deleniti, totam pariatur delectus reiciendis consequatur quas soluta ipsa deserunt sint!",
+
+    date: "9 MAY 2024",
+
+    id: "bLgSTwO",
+  },
+
+  {
+    judul: "LIST PLAN IN ARTWORK",
+
+    subJudul: "ini subjudul atau deskripsi singkat",
+
+    status: "COMING SOON",
+
+    desk1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae totam quos odio ex autem suscipit, nam dolorem reprehenderit ipsum itaque doloribus, alias, saepe unde?",
+
+    desk2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, enim quaerat! Porro minus obcaecati doloremque eum rem, libero aperiam consectetur exercitationem unde, nisi deleniti, totam pariatur delectus reiciendis consequatur quas soluta ipsa deserunt sint!",
+
+    date: "9 MAY 2024",
+
+    id: "bLgSThreE",
+  },
+
+  {
+    judul: "LIST PLAN IN ARTWORK",
+
+    subJudul: "ini subjudul atau deskripsi singkat",
+
+    status: "COMING SOON",
+
+    desk1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae totam quos odio ex autem suscipit, nam dolorem reprehenderit ipsum itaque doloribus, alias, saepe unde?",
+
+    desk2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, enim quaerat! Porro minus obcaecati doloremque eum rem, libero aperiam consectetur exercitationem unde, nisi deleniti, totam pariatur delectus reiciendis consequatur quas soluta ipsa deserunt sint!",
+
+    date: "9 MAY 2024",
+
+    id: "bLgSFouR",
+  },
+
+  {
+    judul: "LIST PLAN IN ARTWORK",
+
+    subJudul: "ini subjudul atau deskripsi singkat",
+
+    status: "COMING SOON",
+
+    desk1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae totam quos odio ex autem suscipit, nam dolorem reprehenderit ipsum itaque doloribus, alias, saepe unde?",
+
+    desk2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, enim quaerat! Porro minus obcaecati doloremque eum rem, libero aperiam consectetur exercitationem unde, nisi deleniti, totam pariatur delectus reiciendis consequatur quas soluta ipsa deserunt sint!",
+
+    date: "9 MAY 2024",
+
+    id: "bLgSFivE",
+  },
 ];
 export default DiaryData;
