d88911cbda2adf0fc8451dbdfcf82df28aaeee42
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-05-27 16:55:15 +0700

    WIP

---
+++ src/app/components/savings/ModalSavings.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+interface ModalSavingsProps {
+  membuka: boolean;
+  konten: string;
+  namaInput: string;
+  menutup: () => void;
+  perubahan: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  menyimpan: () => void;
+}
+
+const ModalSavings: React.FC<ModalSavingsProps> = ({
+  membuka,
+  konten,
+  namaInput,
+  menutup,
+  perubahan,
+  menyimpan,
+}) => {
+  if (!membuka) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg">
+        <h2 className="text-2xl mb-4">{konten}</h2>
+        <input
+          type="number"
+          value={namaInput}
+          onChange={perubahan}
+          placeholder={`Masukkan nominal ${konten.toLowerCase()}`}
+          className="border p-2 w-full mb-4"
+        />
+
+        <button
+          onClick={menyimpan}
+          className="bg-green-500 text-white px-4 py-2 rounded mr-2"
+        >
+          Simpan
+        </button>
+
+        <button
+          onClick={menutup}
+          className="bg-red-500 text-white px-4 py-2 rounded"
+        >
+          Tutup
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalSavings;
--- src/app/components/savings/Savings.tsx
+++ src/app/components/savings/Savings.tsx
@@ -1,11 +1,107 @@
-import React from "react";
+"use client";
+
+import {
+  IconPlus,
+  IconStairsDown,
+  IconStairsUp,
+  IconWallet,
+} from "@tabler/icons-react";
+import React, { useState } from "react";
+import ModalSavings from "./ModalSavings";
 
 const Savings = () => {
+  const [membukaModal, mengaturMembukaModal] = useState(false);
+  const [modalKonten, mengaturModalKonten] = useState("");
+  const [nilaiInput, mengaturNilaiInput] = useState("");
+  const [penghasilan, mengaturPenghasilan] = useState("1.000.000");
+  const [targetPengeluaran, mengaturTargetPengeluaran] = useState("1.000.000");
+  const [totalPengeluaran, mengaturTotalPengeluaran] = useState("0");
+
+  const modalTerbuka = (konten: React.SetStateAction<string>) => {
+    mengaturModalKonten(konten);
+    mengaturNilaiInput("");
+    mengaturMembukaModal(true);
+  };
+
+  const btnTutupModal = () => {
+    mengaturMembukaModal(false);
+    mengaturModalKonten("");
+  };
+
+  const mengubahNilaiInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    mengaturNilaiInput(event.target.value);
+  };
+
+  const btnSave = () => {
+    if (modalKonten === "Penghasilan") {
+      mengaturPenghasilan(nilaiInput);
+    } else if (modalKonten === "Target pengeluaran") {
+      mengaturTargetPengeluaran(nilaiInput);
+    } else if (modalKonten === "Total pengeluaran") {
+      mengaturTotalPengeluaran(nilaiInput);
+    }
+    btnTutupModal();
+  };
+
   return (
-    <div className="flex items-center justify-center">
-      <div>
-        <button></button>
+    <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center gap-20">
+        <div className="flex items-center justify-center p-2 border border-black relative group">
+          <div className="p-2">
+            <IconWallet className="w-14 h-14 stroke-[0.5]" />
+          </div>
+
+          <div className="p-2 text-center">
+            <h1>Penghasilan</h1>
+            <p>{penghasilan}</p>
+          </div>
+
+          <button
+            onClick={() => modalTerbuka("Penghasilan")}
+            className="absolute bg-black bg-opacity-50 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 group-hover:duration-500"
+          >
+            <IconPlus className="w-6 h-6 text-white" />
+          </button>
+        </div>
+
+        <div className="flex items-center justify-center p-2 border border-black relative group">
+          <div className="p-2">
+            <IconStairsUp className="w-14 h-14 stroke-[0.5]" />
+          </div>
+
+          <div className="p-2 text-center">
+            <h1>Target pengeluaran</h1>
+            <p>{targetPengeluaran}</p>
+          </div>
+
+          <button
+            onClick={() => modalTerbuka("Target pengeluaran")}
+            className="absolute bg-black bg-opacity-50 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 group-hover:duration-500"
+          >
+            <IconPlus className="w-6 h-6 text-white" />
+          </button>
+        </div>
+
+        <div className="flex items-center justify-center p-2 border border-black relative group">
+          <div className="p-2">
+            <IconStairsDown className="w-14 h-14 stroke-[0.5]" />
+          </div>
+
+          <div className="p-2 text-center">
+            <h1>Total pengeluaran</h1>
+            <p>{totalPengeluaran}</p>
+          </div>
+        </div>
       </div>
+
+      <ModalSavings
+        membuka={membukaModal}
+        konten={modalKonten}
+        namaInput={nilaiInput}
+        menutup={btnTutupModal}
+        perubahan={mengubahNilaiInput}
+        menyimpan={btnSave}
+      />
     </div>
   );
 };
