bb0f28496703a4a6992acf4f1a3bc9f572040f7a
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-06-01 00:37:34 +0700

    WIP

--- src/app/components/gallery/MyGallery.tsx
+++ src/app/components/gallery/MyGallery.tsx
@@ -1,4 +1,4 @@
-import { IconMoodSad } from "@tabler/icons-react";
+import { IconMoodSad, IconPhotoOff } from "@tabler/icons-react";
 import React from "react";
 
 interface DataFoto {
@@ -15,9 +15,26 @@ interface MyGalleryProps {
 const MyGallery: React.FC<MyGalleryProps> = ({ data, modalTerbuka }) => {
   if (data.length === 0 || data.every((item) => !item.deskripsi.trim())) {
     return (
-      <div className="flex item-center justify-center">
-        <div className="w-[100px] h-[100px] md:w-52 md:h-52 lg:w-60 lg:h-60 xl:w-72 xl:h-72 flex flex-col items-center justify-center border border-black">
-          <h1 className="font-ketiga">NO POSTS</h1>
+      <div className="flex item-center justify-center gap-2 m-2">
+        <div className="w-[100px] h-[100px] md:w-52 md:h-52 lg:w-60 lg:h-60 xl:w-72 xl:h-72 border border-gray-400 flex flex-col items-center justify-center">
+          <IconPhotoOff className="w-8 h-8 md:w-28 md:h-28 stroke-[0.5] text-gray-400" />
+          <h1 className="font-ketiga text-gray-400 text-xs md:text-lg">
+            NO POSTS
+          </h1>
+        </div>
+
+        <div className="w-[100px] h-[100px] md:w-52 md:h-52 lg:w-60 lg:h-60 xl:w-72 xl:h-72 border border-gray-400 flex flex-col items-center justify-center">
+          <IconPhotoOff className="w-8 h-8 md:w-28 md:h-28 stroke-[0.5] text-gray-400" />
+          <h1 className="font-ketiga text-gray-400 text-xs md:text-lg">
+            NO POSTS
+          </h1>
+        </div>
+
+        <div className="w-[100px] h-[100px] md:w-52 md:h-52 lg:w-60 lg:h-60 xl:w-72 xl:h-72 border border-gray-400 flex flex-col items-center justify-center">
+          <IconPhotoOff className="w-8 h-8 md:w-28 md:h-28 stroke-[0.5] text-gray-400" />
+          <h1 className="font-ketiga text-gray-400 text-xs md:text-lg">
+            NO POSTS
+          </h1>
         </div>
       </div>
     );
