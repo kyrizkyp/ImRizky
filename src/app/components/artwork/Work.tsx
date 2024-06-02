cfadc8ba15c938aab6ce2a72e455184ceea076e3
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-06-02 17:47:22 +0700

    WIP

--- src/app/components/artwork/Work.tsx
+++ src/app/components/artwork/Work.tsx
@@ -7,18 +7,18 @@ const karya = [
   {
     judul: "SURABAYA DEV",
     deskripsi:
-      "SurabayaDev, is an IT community with the theme of technology education in synergy and collaboration.",
+      "Updated the appearance of the SurabayaDev IT community website.",
     gambar: "/artwork/sbydev.png",
   },
 ];
 
 const Work = () => {
-  const [selectedImage, setSelectedImage] = useState(karya[0].gambar);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [gambarDipilih, mengaturGambarDipilih] = useState(karya[0].gambar);
+  const [indexAktif, mengaturIndexAktif] = useState(0);
 
-  const klikAktif = (image: string, index: number) => {
-    setSelectedImage(image);
-    setActiveIndex(index);
+  const klikAktif = (gambar: string, index: number) => {
+    mengaturGambarDipilih(gambar);
+    mengaturIndexAktif(index);
   };
 
   return (
@@ -32,21 +32,21 @@ const Work = () => {
       <div className="w-full px-4 md:px-10 xl:px-24 2xl:px-64">
         <div className="flex flex-col md:flex-row items-center md:items-start justify-between mt-4">
           <div className="space-y-4">
-            {karya.map((activity, index) => (
+            {karya.map((potongan, index) => (
               <div
                 key={index}
                 className={`max-w-lg border border-black p-2 cursor-pointer rounded-lg ${
-                  activeIndex === index ? "" : ""
+                  indexAktif === index ? "" : ""
                 }`}
-                onClick={() => klikAktif(activity.gambar, index)}
+                onClick={() => klikAktif(potongan.gambar, index)}
               >
                 <div className="flex items-center">
                   <div className="p-2">
                     <h2 className="p-1 font-ketiga font-extrabold text-lg">
-                      {activity.judul}
+                      {potongan.judul}
                     </h2>
 
-                    <p className="p-1 font-pertama">{activity.deskripsi}</p>
+                    <p className="p-1 font-pertama">{potongan.deskripsi}</p>
                   </div>
                 </div>
               </div>
@@ -60,7 +60,7 @@ const Work = () => {
               loading="eager"
               quality={100}
               priority
-              src={selectedImage}
+              src={gambarDipilih}
               alt="artwork"
               className="object-cover w-full h-full"
             />
