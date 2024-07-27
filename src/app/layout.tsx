9f5ce8e3eea72ffb523f4402c708595ef213320f
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-07-27 17:04:31 +0700

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/navigation/Navbar";
 import Footer from "./components/footer/Footer";
 
 interface CustomMetadata extends Metadata {
-  image?: string;
+  image: string;
 }
 
 export const metadata: CustomMetadata = {
@@ -24,17 +24,6 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/logo/iam.png" sizes="any" />
         <meta property="og:image" content={metadata.image} />
-        <meta property="og:title" content="KYRIZKYP - Creativity Everything" />
-        <meta
-          property="og:description"
-          content="Introducing me Rizky Putra, a Front-End Web Developer, specialist and precision in creating websites or what is usually called landing pages."
-        />
-        <meta
-          property="og:image"
-          content="https://kyrizkyp.vercel.app/logo/iam.png"
-        />
-        <meta property="og:url" content="https://kyrizkyp.vercel.app/" />
-        <meta property="og:type" content="website" />
       </head>
       <body className="flex flex-col min-h-screen">
         <Navbar />
