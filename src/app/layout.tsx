e1b699c72ee783f3d9108a1350a6f79fb452bd12
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-07-27 16:59:49 +0700

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,15 +19,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const description = metadata.description ?? "";
-  const image = metadata.image ?? "";
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/logo/iam.png" sizes="any" />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-        <meta property="og:url" content={image} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:title" content="KYRIZKYP - Creativity Everything" />
+        <meta
+          property="og:description"
+          content="Introducing me Rizky Putra, a Front-End Web Developer, specialist and precision in creating websites or what is usually called landing pages."
+        />
+        <meta
+          property="og:image"
+          content="https://kyrizkyp.vercel.app/logo/iam.png"
+        />
+        <meta property="og:url" content="https://kyrizkyp.vercel.app/" />
+        <meta property="og:type" content="website" />
       </head>
       <body className="flex flex-col min-h-screen">
         <Navbar />
